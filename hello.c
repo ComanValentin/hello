ffe7fb1644480a2374b936c2f05ad0fdfe0ce384
ComanValentin <comanvalentin84@yahoo.com>
2018-04-19 16:20:16 +0300

    V2

--- hello.c
+++ hello.c
@@ -1,8 +1,22 @@
+
+
+
+
+
 #include<stdio.h>
-int main()
+int main(int argc, char *argc[])
+
+
+
 {
+if(argc != 2)
+{
+printf("USAGE: %s name\n",argv[0]);
+return -1;
+}
 
 
-printf ("Hello, World!\n");
+
+printf ("Hello,%s! \n", argv[1]);
 return 0;
 }
